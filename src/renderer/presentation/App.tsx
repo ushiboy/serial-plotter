2b826082bf30bd18f8d16443b11e302485fc2e12
ushiboy <ushiboy.dev@gmail.com>
2021-01-23 22:58:37 +0900

    render serialports list

--- src/renderer/presentation/App.tsx
+++ src/renderer/presentation/App.tsx
@@ -3,8 +3,19 @@ import { AppState } from '../../status';
 
 export const App: React.FC<{ state: AppState }> = (props) => {
   const { state } = props;
+  const { serialPorts } = state;
+
+  const options = serialPorts.map((p) => {
+    return <option key={p}>{p}</option>;
+  });
   return (
     <div className="container">
+      <form>
+        <div className="form-group">
+          <label>Serial port</label>
+          <select className="form-control">{options}</select>
+        </div>
+      </form>
       <h1>💖 Hello World!</h1>
       <p>Welcome to your Electron application.</p>
       <p>{state.version}</p>
