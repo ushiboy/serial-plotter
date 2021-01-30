c45bbbf21b388fe656c8c80bbb1050ab2cd23fcf
ushiboy <ushiboy.dev@gmail.com>
2021-01-30 23:01:03 +0900

    wip

--- src/module/Store.ts
+++ src/module/Store.ts
@@ -1,10 +1,19 @@
 import { AppAction } from './type';
-import { AppState } from '../status';
+import { Model } from './model';
 
-export type Subscriber = (state: AppState) => void;
+export type Subscriber = (state: Model) => void;
 export type Unsubscriber = () => void;
 
 export type Store = {
   dispatch: (action: AppAction) => void;
   subscribe: (subscriber: Subscriber) => Unsubscriber;
+};
+
+export const createStore = (initialState: Model): Store => {
+  return {
+    dispatch(action: AppAction): void {},
+    subscribe(subscriber: Subscriber): Unsubscriber {
+      return (): void => {};
+    },
+  };
 };
