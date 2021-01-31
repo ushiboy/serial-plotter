d9d7ff521236f5d02d151e743290ffce44ad920e
ushiboy <ushiboy.dev@gmail.com>
2021-01-31 23:28:08 +0900

    wip

--- src/module/Store.ts
+++ src/module/Store.ts
@@ -1,15 +1,26 @@
 import { AppAction } from './type';
 import { Model } from './model';
+import { serialUpdater, SerialModel, serialInit } from './Serial';
+import { logUpdater, LogModel } from './Log';
 
 export type Subscriber = (state: Model) => void;
 export type Unsubscriber = () => void;
+
+export type Update<T> = (action: AppAction, model: T, any: any) => Promise<T>;
+
+export type UpdateConfig<T> = {
+  [K in keyof T]: Update<T[K]>;
+};
 
 export type Store = {
   dispatch: (action: AppAction) => void;
   subscribe: (subscriber: Subscriber) => Unsubscriber;
 };
 
-export const createStore = (initialState: Model): Store => {
+export const createStore = (
+  initialState: Model,
+  updateConfigs: UpdateConfig<Model>
+): Store => {
   return {
     dispatch(action: AppAction): void {},
     subscribe(subscriber: Subscriber): Unsubscriber {
@@ -17,3 +28,16 @@ export const createStore = (initialState: Model): Store => {
     },
   };
 };
+
+export const store = createStore(
+  {
+    serial: serialInit(),
+    log: {
+      logs: [],
+    },
+  },
+  {
+    serial: serialUpdater,
+    log: logUpdater,
+  }
+);
